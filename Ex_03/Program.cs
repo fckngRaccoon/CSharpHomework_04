38810bfc8356f5b1a842ad8ab8fe05a614735fc0
cppdev <fckngRaccoon@yandex.ru>
2023-03-12 16:44:39 +0300

    Ex03

---
+++ Ex_03/Program.cs
@@ -0,0 +1,35 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int[] GenerateArray(int Lenght, int minValue, int maxValue)
+{
+    int[] array = new int[Lenght];
+    Random random = new Random();
+    for (int i = 0; i < Lenght; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.WriteLine("]");
+}
+
+int lenght = Prompt("Длина: ");
+int min = Prompt("Начальное значение: ");
+int max = Prompt("Конечное значение: ");
+int[] array = GenerateArray(lenght,min,max);
+PrintArray(array);
